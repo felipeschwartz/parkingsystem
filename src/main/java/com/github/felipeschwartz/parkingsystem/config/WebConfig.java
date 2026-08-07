9b88ac13e6316931a76a2e6f8d9f481458305650
Felipe Schwartz <felipeschwartz@gmail.com>
2026-08-07 10:04:28 -0300

    implemented CORS configuration

---
+++ src/main/java/com/github/felipeschwartz/parkingsystem/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.github.felipeschwartz.parkingsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(org.springframework.http.MediaType.APPLICATION_JSON)
+                .mediaType("json", org.springframework.http.MediaType.APPLICATION_JSON)
+                .mediaType("xml", org.springframework.http.MediaType.APPLICATION_XML)
+                .mediaType("yaml", org.springframework.http.MediaType.APPLICATION_YAML);
+    }
+}
